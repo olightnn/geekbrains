5c7f633283b70077a8d223df2561a5982a214eaa
olightnn <53794151+olightnn@users.noreply.github.com>
2020-01-26 09:46:12 +0300

    Create items.py

---
+++ datamining/ep04/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class GbparseItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    _id = scrapy.Field()
+    name = scrapy.Field()
+    salary = scrapy.Field()
+    link = scrapy.Field()
+    site = scrapy.Field()
+    data = scrapy.Field()
+    pass
