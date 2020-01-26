dcd3d5a8ba583cc71664dad91fb11acfe444a66f
olightnn <53794151+olightnn@users.noreply.github.com>
2020-01-26 09:44:56 +0300

    Create hhru.py

---
+++ datamining/ep04/hhru.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import HtmlResponse
+
+from gbparse.items import GbparseItem
+
+
+class HhruSpider(scrapy.Spider):
+    name = 'hhru'
+    allowed_domains = ['hh.ru']
+    start_urls = [
+        'https://nn.hh.ru/search/vacancy?clusters=true&enable_snippets=true&text=python&showClusters=true']
+
+    def parse(self, response: HtmlResponse):
+        next_page = response.css('a.HH-Pager-Controls-Next::attr(href)').extract_first()
+        yield response.follow(next_page, callback=self.parse)
+
+        vacancy_items = response.css(
+            'div.vacancy-serp div.vacancy-serp-item div.vacancy-serp-item__row_header a.bloko-link::attr(href)').extract()
+        for link in vacancy_items:
+            yield response.follow(link, callback=self.vacancy_parse)
+
+    def format_salary(self, salary):
+        res = "з/п не указана"
+        sal_f = "".join(salary)
+        if "от" in sal_f and "до" in sal_f and len(salary) > 3:
+            res = {"min_salary": salary[1].replace(u'\xa0', ' '), "max_salary": salary[3].replace(u'\xa0', ' ')}
+        elif "от" in sal_f:
+            res = {"min_salary": salary[1].replace(u'\xa0', ' '), "max_salary": "не указано"}
+        elif "до" in sal_f:
+            res = {"min_salary": "не указано", "max_salary": salary[1].replace(u'\xa0', ' ')}
+        return res
+
+    def vacancy_parse(self, response: HtmlResponse):
+        name = response.xpath('//h1[@class=\'header\']//span/text()').extract_first()
+        link = response.url
+        salary = response.css('div.vacancy-title p.vacancy-salary::text').extract()
+        salary = self.format_salary(salary)
+        print(name, link, salary)
+        yield GbparseItem(name=name, salary=salary, link=link, site="hh.ru")
+        
